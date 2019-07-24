5d14e7e1e236d3f2a0f8dd1302404ab589c77a08
Uchey <ucheuzochukwumark@gmail.com>
2019-07-24 22:44:42 +0100

    chore:created a .babelrc file to help transpire to ES6

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+	res.status(200).send({
+		message: 'Welcome to WayFarer-API',
+	});
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+	console.log(`WayFarer-API started on port ${port}`);
+});
+
+export default app;
