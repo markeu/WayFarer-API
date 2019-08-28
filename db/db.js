f65eff117581f9c867d4b4842c86945c57df7f68
Uchey <ucheuzochukwumark@gmail.com>
2019-08-28 18:39:30 +0100

    chore(travis set-up)

--- db/db.js
+++ db/db.js
@@ -4,16 +4,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
 });
 
 pool.on('connect', () => {
   console.log('connected to database');
 });
-
-// const pool = new Pool({
-//   connectionString: process.env.POSTGRES_DB_URL,
-
-// });
 
 export default pool;
